9156209215e039245b8fd4cbc8c082981bc4f820
Jordan Merrill <amanithought@gmail.com>
2018-03-10 22:11:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ var height = 39
 var message = `${name} is ${height} inches tall`
 
 module.exports = {name, height, message}
+https://learn.co/tracks/bootcamp-prep/javascript-fundamentals/functions-and-scope/what-is-a-test-lab#
